7ec2a540d075820380ffbea72b5055309d027b6a
Matt Ericksen <ericksenm93@ymail.com>
2020-07-10 18:36:28 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,8 +1,8 @@
 # code your #position_taken? method here!
 def position_taken?(board, ind)
-  if board[ind] != " "
-    return true
-  else
+  if board[ind] == " " || board[ind] == "" || board[ind] == nil
     return false
+  else
+    return true
   end
 end
